6ca79b48865be1ba03ebaf26d3a919282bcdc0f9
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-17 12:09:04 +0100

    Add functionalty

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-const openMenu = document.querySelector('.nav .close-menu');
+const openMenu = document.querySelector('img .nav');
 const closeMenu = document.querySelector('.mobile-menu .close-menu');
-const menuItems = document.querySelectorAll('.mobile-menu a');
+const menuItems = document.querySelectorAll('.menu-items a');
 
 openMenu.addEventListener('click', () => {
   document.querySelector('.mobile-menu').classList.add('show');
@@ -12,6 +12,75 @@ closeMenu.addEventListener('click', () => {
 
 menuItems.forEach((item) => {
   item.addEventListener('click', () => {
-    document.querySelector('.mobile-menu').classList.remove('show');
+    document.querySelector('.menu-items').classList.remove('show');
   });
+});
+
+
+
+const body=document.querySelector('.see-project');
+open.addEventListener('click', () => {
+    const mainBody=document.createElement('section');
+    const popup=document.createElement('section');
+    popup.innerHTML= '
+        <div class="Pics" id="prof">
+            <img src="imgs/Facebook360.png" alt="Pics">
+        </div>
+        <div class="contentright">
+            <div class="tonic">
+            <h1>Tonic</h1>
+            </div>
+            <div>
+            <ul class="dates">
+                <li class="canopy"> 
+                    <h5>CANOPY</h5>
+                </li>
+                <li class="dot1">
+                <a href="#">
+                    <img src="imgs/Dot.png" alt="dot1">
+                </a>
+                </li>
+                <li class="backEnd">
+                    <h5>Back End Dev</h5>
+                </li>
+                <li class="dot2">
+                <a href="#">
+                    <img src="imgs/Dot.png" alt="dot2">
+                </a>
+                </li>
+                <li class="date">
+                <h5>2015</h5>
+                </li>
+            </ul>
+            </div>
+            <div class="content-details">
+            <p>
+                A daily selection of privately personalized reads;
+                no accounts or sign-ups required.
+            </p>
+            </div>
+            <div class="prog-lists">
+            <ul class="lists">
+                <li>
+                <a href="#"> <h6>html</h6></a>
+                </li>
+                <li>
+                <a href="#"> <h6>Css</h6></a>
+                </li>
+                <li>
+                <a href="#"> <h6>JavaScript</h6></a>
+                </li>
+            </ul>
+            </div>
+            <div class="button">
+            <a class="close" href="https://github.com/basirking/">
+                <h4>See Project</h4>
+            </a>
+            </div>
+        </div> 
+
+    ';
+
+    mainBody.appendChild(popup);
+    body.append(mainBody);
 });
