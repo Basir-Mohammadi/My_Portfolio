2c871f17a0735f3a235d91575f56dd168b701d7a
Bornittah <bornittah@gmail.com.com>
2022-05-17 11:10:46 +0300

    add mobile menu

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-popup');
+const navLinks = document.querySelector('.nav-links');
+const logo = document.querySelector('.logo');
+
+hamburger.addEventListener('click', ()=>{
+hamburger.classList.toggle('active');
+menu.classList.toggle('active');
+navLinks.classList.toggle('active');
+logo.classList.toggle('active');
+});
+
+document.querySelector('.nav-link')
+.forEach(n=>n.addEventListener('click', ()=>{
+hamburger.classList.remove('active');
+navLinks.classList.remove('active');
+logo.classList.remove('active');
+// logo.style.display="none";
+// logo.style.display="none";
+}));
