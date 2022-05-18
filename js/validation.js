3fe935cc32899a82e1316f9c09da1c208be2f86d
shaheer ch <shaheer.chry@gmail.com>
2022-05-18 16:52:50 +0500

    Update Validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,114 +1,29 @@
-const form = document.getElementById('form');
-const fname = document.getElementById('fname');
-const email = document.getElementById('email');
-const text = document.getElementById('text');
-const button = document.getElementById('btn');
+const Form = document.getElementById("forms");
+const email = document.getElementById("email");
 
+const validation = (event) => {
+  const emailError = document.querySelector(".error");
 
-form.addEventListener('submit', (e) => {
-  e.defaultPrevented();
-
-  validInputs();
-
-});
-
-btn.addEventListener('click', (e) => {
-    e.defaultPrevented();
-    validInputs();
-})
-
-const setError = (Element,message) => {
-    const inputcontrol = Element.parentElement;
-    const errordisplay= inputcontrol.queryselectro('.error');
-
-    errordisplay.innerText =message;
-
-    inputcontrol.classlist.add('error');
-    inputcontrol.classlist.remove('success');
-
-}
-
-
-const setSuccess =Element => {
-    
-    const inputcontrol = Element.parentElement;
-    const errordisplay= inputcontrol.queryselectro('.error');
-
-    errordisplay.innerText =' ';
-    inputcontrol.classlist.remove('error');
-    inputcontrol.classlist.add('success');
-
-
-}
-
-const isValidemail =email =>{
-    const re=/ [a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-    return re.test(String(email).toLowerCase());
-}
-
-
-const validInputs = () => {
-    const firstname = fname.value.trim();
-    const emailvalue = email.value.trim();
-    const textvalue = text.value.trim();
-
-    if (firstname.length == 0){
-
-        setError =(fname,'name required..');
-    }else{
-        setSuccess(fname);
-    }
-
-    if (emailvalue.length == 0){
-        setError(email,'email required');
-
-    }else if(!isValidemail(emailvalue)){
-        setError(email,'provide a valid email..');
-    }
-    else{
-        setSuccess(email)
-    }
-
+  if (email.validity.patternMismatch) {
+    email.setCustomValidity("");
+    emailError.textContent = "Enter email in lowercase";
+    emailError.classList.add("show");
+    event.preventDefault();
+  } else if (email.validity.valueMissing) {
+    email.setCustomValidity("");
+    emailError.textContent = "Kindly enter email";
+    emailError.classList.add("show");
+    event.preventDefault();
+  } else if (email.validity.typeMismatch) {
+    email.setCustomValidity("");
+    emailError.textContent = "Kindly enter proper email";
+    emailError.classList.add("show");
+    event.preventDefault();
+  } else {
+    email.setCustomValidity("");
+    emailError.classList.remove("show");
+    event.preventDefault();
+  }
 };
 
-
-
-// function validate()
-// {
-
-//    if( document.myForm.firstname.value == "" )
-//    {
-//      alert( "Please provide your name!" );
-//      document.myForm.firstname.focus() ;
-//      return false;
-//    }
-//    if( document.myForm.email.value == "" )
-//    {
-//      alert( "Please provide your Email!" );
-//      document.myForm.email.focus() ;
-//      return false;
-//    }
-//    return( true );
-// }
-
-// function validateForm() {
-
-//     var x=document.forms["myForm"]["fname"].value;
-//     if (x==null || x=="")
-//      {
-//       alert("Name must be filled out");
-//       return false;
-//      }
-    
-//     var y=document.forms["myForm"]["email"].value;
-//     if (y==null || y=="") {
-//       alert("Password name must be filled out");
-//       return false;
-//     }
-
-//     var y=document.forms["myForm"]["text"].value;
-//     if (y==null || y=="") {
-//       alert("Password name must be filled out");
-//       return false;
-//     }
-// }
+Form.addEventListener("input", validation);
