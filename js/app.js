8a69c6f873b7a05153b6891e0635702b89e712ec
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-18 14:30:32 +0100

    Add eslint

--- js/app.js
+++ js/app.js
@@ -3,19 +3,17 @@ const menu = document.querySelector('.menu-popup');
 const navLinks = document.querySelector('.nav-links');
 const logo = document.querySelector('.logo');
 
-hamburger.addEventListener('click', ()=>{
-hamburger.classList.toggle('active');
-menu.classList.toggle('active');
-navLinks.classList.toggle('active');
-logo.classList.toggle('active');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+  navLinks.classList.toggle('active');
+  logo.classList.toggle('active');
 });
 
 document.querySelector('.nav-links')
-.forEach(n=>n.addEventListener('click', ()=>{
-hamburger.classList.remove('active');
-navLinks.classList.remove('active');
-logo.classList.remove('active');
-menu.classList.remove('active');
-// logo.style.display="none";
-// logo.style.display="none";
-}));
+  .forEach(navLinks, () => navLinks.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navLinks.classList.remove('active');
+    logo.classList.remove('active');
+    menu.classList.remove('active');
+  }));
