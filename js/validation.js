2b6bbc5cf144e5dadf7c03344fe01210046bdb43
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-18 14:09:20 +0100

    Add eslint

--- js/validation.js
+++ js/validation.js
@@ -1,29 +1,28 @@
-const Form = document.getElementById("forms");
-const email = document.getElementById("email");
+const Form = document.getElementById('forms');
+const email = document.getElementById('email');
 
 const validation = (event) => {
-  const emailError = document.querySelector(".error");
+  const emailError = document.querySelector('.error');
 
   if (email.validity.patternMismatch) {
-    email.setCustomValidity("");
-    emailError.textContent = "Enter email in lowercase";
-    emailError.classList.add("show");
+    email.setCustomValidity('');
+    emailError.textContent = 'Enter email in lowercase';
+    emailError.classList.add('show');
     event.preventDefault();
   } else if (email.validity.valueMissing) {
-    email.setCustomValidity("");
-    emailError.textContent = "Kindly enter email";
-    emailError.classList.add("show");
+    email.setCustomValidity('');
+    emailError.textContent = 'Kindly enter email';
+    emailError.classList.add('show');
     event.preventDefault();
   } else if (email.validity.typeMismatch) {
-    email.setCustomValidity("");
-    emailError.textContent = "Kindly enter proper email";
-    emailError.classList.add("show");
+    email.setCustomValidity('');
+    emailError.textContent = 'Kindly enter proper email';
+    emailError.classList.add('show');
     event.preventDefault();
   } else {
-    email.setCustomValidity("");
-    emailError.classList.remove("show");
+    email.setCustomValidity('');
+    emailError.classList.remove('show');
     event.preventDefault();
   }
 };
-
-Form.addEventListener("input", validation);
+Form.addEventListener('input', validation);
