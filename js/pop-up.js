e7dac94420bc8616b97d00eb3355e10fe6984cc1
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-19 15:36:17 +0100

    Add new functions

--- js/pop-up.js
+++ js/pop-up.js
@@ -120,7 +120,7 @@ projects.forEach((obj) => {
     </ul>
   </div>
 
-  <div class="button">
+  <div class="button seeproject">
     <a href="#">
       <h4>${obj.seeproject}</h4>
     </a>
@@ -131,3 +131,62 @@ projects.forEach((obj) => {
 `;
 });
 section.innerHTML = data;
+
+const open = document.querySelector('.seeproject');
+
+open.addEventListener('click', () => {
+  let data = '';
+  const section = document.querySelector('.projects');
+  projects.forEach((obj) => {
+    data += `<section class="container-1" id="About">
+  <div class="Pics" id="prof">
+  <img src="${obj.image}" alt="Pics" />
+</div>
+<div class="contentright">
+  <div class="tonic">
+    <h1>${obj.title}</h1>
+  </div>
+  <div>
+    <ul class="dates">
+    <li class="canopy">
+    <h5>${obj.roles.company}</h5>
+  </li>
+  <li class="dot1">
+    <a href="#">
+      <img src="imgs/Dot.png" alt="dot1" />
+    </a>
+  </li>
+  <li class="backEnd">
+    <h5>${obj.roles.position}</h5>
+  </li>
+      </li>
+    </ul>
+  </div>
+  <div class="content-details">
+    <p>
+     ${obj.description}
+    </p>
+  </div>
+  <div class="prog-lists">
+    <ul class="lists">
+      <li>
+        <a href="#">
+        <h6>${obj.skills}</h6>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="button seeproject">
+    <a href="#">
+      <h4>${obj.seeproject}</h4>
+    </a>
+  </div>
+</div>
+</section>
+
+`;
+  });
+
+  section.innerHTML = data;
+});
