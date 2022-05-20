82cc8a4557bb2706ef0a59b60358675324635d8e
shaheer ch <shaheer.chry@gmail.com>
2022-05-21 00:44:19 +0500

    Final edit

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,79 +1,33 @@
-
 /* eslint-disable linebreak-style */
 /* eslint-disable require-jsdoc */
 
-const name = document.querySelector('fname');
-const email1 = document.querySelector('email');
-const message = document.querySelector('textarea');
+const formData = {};
+const appForms = document.forms[0];
+const userName = appForms.elements.username;
+const userEmail = appForms.elements.email;
+const userMessage = appForms.elements.message;
 
-function storeValues(name, email1, message) {
-  const dataObject = JSON.stringify({ name, email1, message });
-  localStorage.setItem('dataObject', dataObject);
-}
+const localStorageSave = (itemKey, ItemValue) => {
+  localStorage.setItem(itemKey, ItemValue);
+};
 
-function populateStorage() {
-  const formValues = JSON.parse(localStorage.getItem('dataObject'));
-  if (formValues) {
-    name.value = formValues.name;
-    email1.value = formValues.email1;
-    message.value = formValues.message;
+const saveUserInfo = () => {
+  formData.email = userEmail.value;
+  formData.username = userName.value;
+  formData.message = userMessage.value;
+  localStorageSave("data", JSON.stringify(formData));
+};
+
+userEmail.addEventListener("blur", saveUserInfo);
+userName.addEventListener("blur", saveUserInfo);
+userMessage.addEventListener("blur", saveUserInfo);
+
+const displayUserInfo = () => {
+  if (localStorage.getItem("data")) {
+    const userInfo = JSON.parse(localStorage.getItem("data"));
+    userName.value = userInfo.username;
+    userEmail.value = userInfo.email;
+    userMessage.value = userInfo.message;
   }
-}
-
-function getValues() {
-  name.addEventListener('input', () => storeValues(name.value, email1.value, message.value));
-  email1.addEventListener('input', () => storeValues(name.value, email1.value, message.value));
-  message.addEventListener('input', () => storeValues(name.value, email1.value, message.value));
-}
-
-populateStorage();
-getValues();
-
-
-
-
-
-// const name = document.getElementById('fname');
-// // const email = document.getElementById('email');
-// const message = document.getElementById('text');
-// const button = document.getElementById('btn');
-
-// button.onclick = function () {
-//   const user = name.value;
-//   const mail = email.value;
-//   const sms = message.value;
-
-//   if (user && mail && sms) {
-//     localStorage.setItem(user, mail, sms);
-//   }
-// };
-
-// const firstName = document.querySelector('#fname');
-//  const email = document.querySelector('#email');
-// const message = document.querySelector('#text');
-// function storeData() {
-//   let Dataform = {
-//     'fname': firstName.value,
-//     'email': email.value,
-//     'message': message.value
-// }
-
-//   localStorage.setItem('form-data', JSON.stringify(Dataform));
-// }
-// function fetchData(){
-//   let data=localStorage.getItem('form-data');
-//     if(data){
-//         fetch=JSON.parse(data);
-//         firstName.value = fetch.firstname;
-//         email.value = fetch.email;
-//         message.text = fetch.messsage;
-//     }
-// }
-
-// window.onload = () => {
-//   firstName.addEventListener('input', storeData());
-//   email.addEventListener('input', storeData());
-//   message.addEventListener('input', storeData());
-//   fetchData();
-// };
-
+};
+displayUserInfo();
