5783ea54d6d65ab8ef393a94585d3aa2d7993a7d
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-20 20:10:45 +0100

    Add local storage functions

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,7 +1,3 @@
-
-/* eslint-disable linebreak-style */
-/* eslint-disable require-jsdoc */
-
 const name = document.querySelector('fname');
 const email1 = document.querySelector('email');
 const message = document.querySelector('textarea');
@@ -29,51 +25,4 @@ function getValues() {
 populateStorage();
 getValues();
 
-
-
-
-
-// const name = document.getElementById('fname');
-// // const email = document.getElementById('email');
-// const message = document.getElementById('text');
-// const button = document.getElementById('btn');
-
-// button.onclick = function () {
-//   const user = name.value;
-//   const mail = email.value;
-//   const sms = message.value;
-
-//   if (user && mail && sms) {
-//     localStorage.setItem(user, mail, sms);
-//   }
-// };
-
-// const firstName = document.querySelector('#fname');
-//  const email = document.querySelector('#email');
-// const message = document.querySelector('#text');
-// function storeData() {
-//   let Dataform = {
-//     'fname': firstName.value,
-//     'email': email.value,
-//     'message': message.value
-// }
-
-//   localStorage.setItem('form-data', JSON.stringify(Dataform));
-// }
-// function fetchData(){
-//   let data=localStorage.getItem('form-data');
-//     if(data){
-//         fetch=JSON.parse(data);
-//         firstName.value = fetch.firstname;
-//         email.value = fetch.email;
-//         message.text = fetch.messsage;
-//     }
-// }
-
-// window.onload = () => {
-//   firstName.addEventListener('input', storeData());
-//   email.addEventListener('input', storeData());
-//   message.addEventListener('input', storeData());
-//   fetchData();
-// };
 
